649a9964b983e1dc358ea41541d54a8a39da4766
Trex1888 <harpua123@gmail.com>
2020-05-23 09:44:01 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import CartContainer from "./components/CartContainer";
+
+import { createStore } from "redux";
+import reducer from "./reducer";
+import { Provider } from "react-redux";
+
+const store = createStore(reducer);
+
+function App() {
+  return (
+    <Provider store={store}>
+      <Navbar />
+      <CartContainer />
+    </Provider>
+  );
+}
+
+export default App;
